e1039184a45ca59e7183dc1e276249cf8c6a0813
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-04-30 05:56:55 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,28 +21,3 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 
 
 
-# 移植RK3399 R08
-
-echo -e "\\ndefine Device/rk3399_r08
-  DEVICE_VENDOR := RK3399
-  DEVICE_MODEL := R08
-  SOC := rk3399
-  DEVICE_DTS := rk3399-r08
-  UBOOT_DEVICE_NAME := r08-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += rk3399_r08" >> target/linux/rockchip/image/armv8.mk
-
-cp -f $GITHUB_WORKSPACE/configfiles/Makefile package/boot/uboot-rockchip/Makefile
-
-cp -f $GITHUB_WORKSPACE/configfiles/r08-rk3399_defconfig package/boot/uboot-rockchip/src/configs/r08-rk3399_defconfig
-
-
-
-
-# 创建rk3399 dts设备树文件夹
-mkdir -p target/linux/rockchip/dts/rk3399
-
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399.dtsi target/linux/rockchip/dts/rk3399/rk3399.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-opp.dtsi target/linux/rockchip/dts/rk3399/rk3399-opp.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-r08.dts target/linux/rockchip/dts/rk3399/rk3399-r08.dts
