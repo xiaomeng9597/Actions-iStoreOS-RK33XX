b6e166179669e1afb59ec78330a7ba39b545e670
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-04-30 07:52:10 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 # sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
 sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
 sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
-cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
+# cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # ls package/base-files/files/etc/
 
 
@@ -27,15 +27,26 @@ echo -e "\\ndefine Device/rk3399_r08
   DEVICE_VENDOR := RK3399
   DEVICE_MODEL := R08
   SOC := rk3399
+  SUPPORTED_DEVICES := rk3399,r08
   DEVICE_DTS := rk3399-r08
   UBOOT_DEVICE_NAME := r08-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += rk3399_r08" >> target/linux/rockchip/image/armv8.mk
 
+
+
+
+
+
+
 cp -f $GITHUB_WORKSPACE/configfiles/Makefile package/boot/uboot-rockchip/Makefile
 
+
 cp -f $GITHUB_WORKSPACE/configfiles/r08-rk3399_defconfig package/boot/uboot-rockchip/src/configs/r08-rk3399_defconfig
+
+
+sed -i "s/armsom,p2pro)/armsom,p2pro|\\\\\n	rk3399,r08)/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 
 
